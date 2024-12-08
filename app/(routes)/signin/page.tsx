68ac6e1127e0d98e0de8ee0ddac80d7f5436c914
Adeolu Amole <adeoluamole@gmail.com>
2024-12-08 07:49:45 +0100

    Bugfix: Production bug fixed on HeroSection component, sign in page, sidebar and navbar component

--- app/(routes)/signin/page.tsx
+++ app/(routes)/signin/page.tsx
@@ -1,122 +1,3 @@
-// import React from "react";
-// import image from "@/public/images/login.png";
-// import Image from "next/image";
-// import { FcGoogle } from "react-icons/fc";
-// import logo from "@/public/images/logo-2.png";
-// import Link from "next/link";
-
-// const SignInPage = () => {
-//   return (
-//     <div className="flex h-screen w-full">
-//       {/* Left Section */}
-//       <div className="flex-1 bg-white flex flex-col justify-center items-center p-8">
-//         <div className="w-full max-w-md">
-//           {/* Logo */}
-//           <div className="flex justify-center mb-2">
-//             <div className="font-bold flex items-center">
-//               <Link href="/">
-//                 <Image
-//                   src={logo}
-//                   alt="RichSea Logo"
-//                   priority
-//                   quality={100}
-//                   //   width={200} // Default width for larger screens
-//                   //   height={30} // Defined height
-
-//                   className="object-contain w-[80px] md:w-[100px]" // Updates width for medium and larger screens
-//                 />
-//               </Link>
-//             </div>
-//           </div>
-//           {/* Title */}
-//           <h1 className="text-2xl font-bold text-center mb-4 text-rich-sea-blue font-raleway">
-//             Sign in to RichSea
-//           </h1>
-//           {/* Wallet Button */}
-//           <button className="w-full bg-black text-white py-2 rounded-lg mb-4 flex justify-center items-center text-sm font-bold font-raleway">
-//             Connect your wallet
-//           </button>
-//           {/* Google Sign-in Button */}
-//           <button className="w-full border border-gray-300 py-2 rounded-lg mb-2 flex justify-center items-center text-sm font-bold font-raleway">
-//             <FcGoogle size={22} />
-//             <span className="ml-2">Sign in with Google</span>
-//           </button>
-//           {/* Separator */}
-//           <div className="text-center text-gray-500 my-4 text-sm font-[700]">
-//             Or
-//           </div>
-//           {/* Email Input */}
-//           <input
-//             type="email"
-//             placeholder="etienodouglas@gmail.com"
-//             className="w-full border border-gray-300 rounded-lg px-4 py-2 mb-4"
-//           />
-//           {/* Password Input */}
-//           <input
-//             type="password"
-//             placeholder="Password"
-//             className="w-full border border-gray-300 rounded-lg px-4 py-2 mb-4 bg-red-100"
-//           />
-//           {/* Sign-in Button */}
-//           <button className="w-full bg-rich-sea-sky text-white py-2 rounded-lg mb-4 hover:bg-rich-sea-sky-hover font-bold font-raleway text-lg">
-//             Sign In
-//           </button>
-
-//           {/* Forgot Password */}
-//           <div className="flex justify-between mb-6">
-//             <span className="text-red-500 text-sm mb-4 italic font-bold">
-//               Your password is incorrect!
-//             </span>
-
-//             <Link
-//               href="/forgot-password"
-//               className="font-raleway font-bold text-[##929292] text-sm"
-//             >
-//               Forgot Password?
-//             </Link>
-//             {/* Error Message */}
-//           </div>
-//           {/* Sign-up Link */}
-//           <p className="text-center text-gray-500 text-sm font-bold">
-//             Don&apos;t have an account?{" "}
-//             <Link
-//               href="/signup"
-//               className="text-rich-sea-yellow font-raleway font-bold"
-//             >
-//               Sign Up here
-//             </Link>
-//           </p>
-//         </div>
-//       </div>
-
-//       {/* Right Section */}
-//       <div
-//         className="flex-1 hidden lg:flex justify-center items-center relative"
-//         style={{
-//           borderTopRightRadius: "30px",
-//           borderBottomRightRadius: "30px",
-//           overflow: "hidden", // Ensures image respects border radius
-//         }}
-//       >
-//         <Image
-//           src={image}
-//           alt="Login Illustration"
-//           fill // Replaces layout="fill"
-//           style={{
-//             objectFit: "cover", // Ensures the image covers the container
-//             objectPosition: "center", // Centers the image
-//           }}
-//         />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default SignInPage;
-
-
-
-
 'use client'
 
 
@@ -312,3 +193,170 @@ const SignInPage = () => {
 };
 
 export default SignInPage;
+
+
+
+
+
+
+
+
+
+// import React from "react";
+// import Image from "next/image";
+// import Link from "next/link";
+// import { Formik, Field, Form, ErrorMessage } from "formik";
+// import { FcGoogle } from "react-icons/fc";
+// import image from "@/public/images/login.png";
+// import logo from "@/public/images/logo-2.png";
+// import { signInValidationSchema } from "@/app/validations/signinValidationSchemas";
+// import { MdAccountBalanceWallet } from "react-icons/md";
+
+// const SignInPage = () => {
+//   return (
+//     <div className="flex h-screen w-full">
+//       {/* Left Section */}
+//       <div className="flex-1 bg-white flex flex-col justify-center items-center p-8">
+//         <div className="w-full max-w-md">
+//           {/* Logo */}
+//           <div className="flex justify-center mb-2">
+//             <div className="font-bold flex items-center">
+//               <Link href="/">
+//                 <Image
+//                   src={logo}
+//                   alt="RichSea Logo"
+//                   priority
+//                   quality={100}
+//                   className="object-contain w-[80px] md:w-[100px]"
+//                 />
+//               </Link>
+//             </div>
+//           </div>
+
+//           {/* Title */}
+//           <h1 className="text-2xl font-bold text-center mb-4 text-rich-sea-blue font-raleway">
+//             Sign in to RichSea
+//           </h1>
+
+//           {/* Wallet Button */}
+//           <button className="w-full bg-black text-white py-2 rounded-lg mb-4 flex justify-center items-center text-sm font-bold font-raleway">
+//             <MdAccountBalanceWallet size={22} /> Connect your wallet
+//           </button>
+
+//           {/* Google Sign-in Button */}
+//           <button className="w-full border border-gray-300 py-2 rounded-lg mb-2 flex justify-center items-center text-sm font-bold font-raleway">
+//             <FcGoogle size={22} />
+//             <span className="ml-2">Sign in with Google</span>
+//           </button>
+
+//           {/* Separator */}
+//           <div className="text-center text-gray-500 my-4 text-sm font-[700]">
+//             Or
+//           </div>
+
+//           {/* Form */}
+//           <Formik
+//             initialValues={{
+//               email: "",
+//               password: "",
+//             }}
+//             validationSchema={signInValidationSchema}
+//             onSubmit={(values) => {
+//               // Handle form submission
+//               console.log("Form submitted:", values);
+//             }}
+//           >
+//             {({ touched, errors }) => (
+//               <Form>
+//                 {/* Email Input */}
+//                 <Field
+//                   type="email"
+//                   name="email"
+//                   placeholder="etienodouglas@gmail.com"
+//                   className={`w-full border ${
+//                     touched.email && errors.email
+//                       ? "border-red-500"
+//                       : "border-gray-300"
+//                   } rounded-lg px-4 py-2 mb-4`}
+//                 />
+//                 <ErrorMessage
+//                   name="email"
+//                   component="div"
+//                   className="text-red-500 text-sm italic font-bold font-raleway"
+//                 />
+
+//                 {/* Password Input */}
+//                 <Field
+//                   type="password"
+//                   name="password"
+//                   placeholder="Password"
+//                   className={`w-full border ${
+//                     touched.password && errors.password
+//                       ? "border-red-500"
+//                       : "border-gray-300"
+//                   } rounded-lg px-4 py-2 mb-4`}
+//                 />
+//                 <ErrorMessage
+//                   name="password"
+//                   component="div"
+//                   className="text-red-500 text-sm italic font-bold font-raleway"
+//                 />
+
+//                 {/* Sign-in Button */}
+//                 <button
+//                   type="submit"
+//                   className="w-full bg-rich-sea-sky text-white py-2 rounded-lg mb-4 hover:bg-rich-sea-sky-hover font-bold font-raleway text-lg"
+//                 >
+//                   Sign In
+//                 </button>
+//               </Form>
+//             )}
+//           </Formik>
+
+//           {/* Forgot Password */}
+//           <div className="flex justify-between mb-6">
+//             <Link
+//               href="/forgot-password"
+//               className="font-raleway font-bold text-[#929292] text-sm"
+//             >
+//               Forgot Password?
+//             </Link>
+//           </div>
+
+//           {/* Sign-up Link */}
+//           <p className="text-center text-gray-500 text-sm font-bold">
+//             Don&apos;t have an account?{" "}
+//             <Link
+//               href="/signup"
+//               className="text-rich-sea-yellow font-raleway font-bold"
+//             >
+//               Sign Up here
+//             </Link>
+//           </p>
+//         </div>
+//       </div>
+
+//       {/* Right Section */}
+//       <div
+//         className="flex-1 hidden lg:flex justify-center items-center relative"
+//         style={{
+//           borderTopRightRadius: "30px",
+//           borderBottomRightRadius: "30px",
+//           overflow: "hidden",
+//         }}
+//       >
+//         <Image
+//           src={image}
+//           alt="Login Illustration"
+//           fill
+//           style={{
+//             objectFit: "cover",
+//             objectPosition: "center",
+//           }}
+//         />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default SignInPage;
